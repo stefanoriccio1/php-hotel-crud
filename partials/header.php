e3b24d2a2b9154766406bdcc33d9cf023bc65657
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-04 18:33:47 +0100

    up

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <link rel="stylesheet" href="dist/app.css">
+    <link rel="stylesheet" href="http://localhost:8888/php-hotel-crud/dist/app.css">
     <meta charset="utf-8">
     <title>Document</title>
   </head>
