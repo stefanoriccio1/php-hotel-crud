c33ddf28ba86f032fefc5526b0d16947a1ddd7c0
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-04 17:50:47 +0100

    up

--- index.php
+++ index.php
@@ -10,30 +10,37 @@ include 'database.php';
     <title></title>
   </head>
   <body>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Floor</th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        if(!empty($rooms)){
-          foreach ($rooms as $room) { ?>
-            <tr>
-              <td><?php echo $room['id'] ?></td>
-              <td><?php echo $floor['id'] ?></td>
-              <td><a href= "">VIEW</a></td>
-              <td><a href= "">UPDATE</a></td>
-              <td>DELETE</td>
-            </tr>
-          <?php }
-        } ?>
-      </tbody>
-    </table>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>Stanze</h1>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Floor</th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              if(!empty($rooms)){
+                foreach ($rooms as $room) { ?>
+                  <tr>
+                    <td><?php echo $room['id'] ?></td>
+                    <td><?php echo $room['floor'] ?></td>
+                    <td><a href= "">VIEW</a></td>
+                    <td><a href= "">UPDATE</a></td>
+                    <td>DELETE</td>
+                  </tr>
+                <?php }
+              } ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
