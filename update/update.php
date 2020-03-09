bea32af6017c3c0d8653b50597338274757761b4
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-09 01:15:04 +0100

    up di update

--- update/update.php
+++ update/update.php
@@ -1,6 +1,20 @@
 <?php
+ include 'server.php';
  include __DIR__ . '/../partials/header.php';
  include __DIR__ . '/../partials/footer.php';
+
+ if(!empty($_GET['id'])){
+   $idRoom = $_GET['id'];
+ }
+
+ $sql = " SELECT * from `stanze` WHERE `id` = $idRoom ";
+ $result = $conn ->query($sql);
+
+
+ if ($result && $result->num_rows > 0) {
+   $room = $result->fetch_assoc();
+
+ }
 ?>
 
 <div class="container">
@@ -9,18 +23,18 @@
       <form action="server.php" method="POST">
         <div class="form-group">
           <label for="room_number">Numero Stanza</label>
-          <input class="form-control" type="text" name="room_number" value="">
+          <input class="form-control" type="text" name="room_number" value="<?php echo $room['room_number'] ?>">
         </div>
         <div class="form-group">
           <label for="floor">Piano</label>
-          <input class="form-control" type="text" name="floor" value="">
+          <input class="form-control" type="text" name="floor" value="<?php echo $room['floor'] ?>">
         </div>
         <div class="form-group">
           <label for="beds">Numero Letti</label>
-          <input  class="form-control"type="text" name="beds" value="">
+          <input  class="form-control"type="text" name="beds" value="<?php echo $room['beds'] ?>">
         </div>
         <div class="form-group">
-          <input type="hidden" name="id" value="">
+          <input type="hidden" name="id" value="<?php echo $room['id'] ?>">
           <input class="form-control" class= "btn btn-submit" type="submit" value="Salva">
         </div>
       </form>
