eb1e41ad03fee34e290e18ab85676d6302e33306
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-10 09:20:12 +0100

    up

---
+++ create/server.php
@@ -0,0 +1,18 @@
+<?php
+  include_once __DIR__ . '/../env.php';
+  include 'database.php';
+
+  if(empty($_POST['beds'])){
+    die('Number of beds no inserted');
+  }
+  if(empty($_POST['room_number'])){
+    die('Room number no inserted');
+  }
+  if(empty($_POST['floor'])){
+    die('Floor no inserted');
+  }
+
+  $sql = "INSERT INTO `stanze` (`beds`, `floor`, `room_number`, `created_at`, `updated_at`) VALUES (?,?,?, NOW(), NOW())";
+
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("iii");
